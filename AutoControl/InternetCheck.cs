68d43982a56fa2d520d319baad88c7ad2118a2c9
Ali Pourmohammad <dexkrad@outlook.com>
2024-05-02 22:46:50 +0330

    V 2.0.0 new UI new Code

---
+++ AutoControl/InternetCheck.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.NetworkInformation;
+
+namespace AutoControl
+{
+    static class InternetCheck
+    {
+        public static bool Internet_test()
+        {
+            Ping ping = new Ping();
+            try
+            {
+                PingReply reply = ping.Send("www.google.com");
+                if (reply.Status == IPStatus.Success)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
--- AutoControl/MainWindow.xaml.cs
+++ AutoControl/MainWindow.xaml.cs
@@ -34,25 +34,59 @@ namespace AutoControl
             InitializeNotifyIcon();
         }
 
+        private void text_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                String txtname;
+                TextBlock x = (TextBlock)sender;
+                txtname = x.Name;
+                TextBox txt = (TextBox)FindName(txtname.Replace("text", "txt"));
+                txt.Focus();
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+        private void txt_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                String textname;
+                TextBox txt = (TextBox)sender;
+                textname = txt.Name;
+                TextBlock text = (TextBlock)FindName(textname.Replace("txt", "text"));
+
+                if (!string.IsNullOrEmpty(txt.Text) && txt.Text.Length > 0)
+                {
+                    text.Visibility = Visibility.Hidden;
+                }
+                else
+                {
+                    text.Visibility = Visibility.Visible;
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
         private void InitializeNotifyIcon()
         {
             notifyIcon = new NotifyIcon();
-            notifyIcon.Icon = new System.Drawing.Icon(@"assest\img\icons8-off-48.ico"); // مسیر آیکون برنامه‌ی‌تان را اینجا قرار دهید
+            notifyIcon.Icon = new System.Drawing.Icon(@"assest\img\icons8-off-48.ico"); 
             notifyIcon.Visible = true;
             notifyIcon.Text = "AC";
             notifyIcon.Click += NotifyIcon_Click;
         }
         private void NotifyIcon_Click(object sender, EventArgs e)
         {
-            // نمایش پنجره اصلی در صورت مخفی بودن
             this.Show();
             this.WindowState = WindowState.Normal;
         }
         private void btnHide_Click(object sender, RoutedEventArgs e)
         {
-            // مخفی کردن پنجره اصلی
             this.Hide();
-            // نمایش آیکون در منوی hidden icons
             notifyIcon.Visible = true;
         }
        
@@ -61,25 +95,37 @@ namespace AutoControl
             string username = Settings.Default.Username;
             txtUsername.Text = username;
         }
-        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
         {
-           
-            Reset2();
+            this.DragMove();
         }
 
-        private async  void btnStart_Click(object sender, RoutedEventArgs e)
+        private void btnStart_Click(object sender, RoutedEventArgs e)
         {
-            if (saveinfo .IsChecked == true)
+            if (rbtnHibernet.IsChecked == false && rbtnShutdown.IsChecked == false)
             {
-                Settings.Default.Username = txtUsername.Text;
-                Settings.Default.Save();
+                MessageBox.Show("لطفا نوع عملیات را انتخاب کنید");
+                return;
             }
             check = true;
+            ChechStatus();
+        }
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            check = false;
+        }
 
+        private async void ChechStatus()
+        {
+            if (InternetCheck.Internet_test() == false)
+            {
+                MessageBox.Show("No Internet");
+                return;
+            }
             try
             {
                 lblStatus.Content = "Status : Cheking Status";
-                var url = "http://autocontrol.freehost.io/getStatus.php"; // آدرس کامل کد PHP خود را اینجا قرار دهید
+                var url = "http://autocontrol.freehost.io/getStatus.php";
                 while (true)
                 {
                     if (check == false)
@@ -87,14 +133,12 @@ namespace AutoControl
                         lblStatus.Content = "Status : Stop Check";
                         return;
                     }
-                    // ایجاد یک instance از HttpClient
                     using (var httpClient = new HttpClient())
                     {
-                        // ایجاد داده‌های فرم برای ارسال
                         var formData = new FormUrlEncodedContent(new[]
                         {
-                    new KeyValuePair<string, string>("username", txtUsername.Text), // جایگزین your_username با نام کاربری موردنظر خود کنید
-                    new KeyValuePair<string, string>("password", psbPassword.Password)  // جایگزین your_password با رمز عبور موردنظر خود کنید
+                    new KeyValuePair<string, string>("username", txtUsername.Text),
+                    new KeyValuePair<string, string>("password", txtPassword.Text)
                 });
 
                         // ارسال درخواست POST و دریافت پاسخ
@@ -107,7 +151,7 @@ namespace AutoControl
                             check = false;
                             Reset();
                         }
-                        else if(responseContent == "User not found or incorrect password.")
+                        else if (responseContent == "User not found or incorrect password.")
                         {
                             check = false;
                             MessageBox.Show(responseContent);
@@ -123,20 +167,23 @@ namespace AutoControl
         }
         private async void Reset()
         {
+            if (InternetCheck.Internet_test() == false)
+            {
+                MessageBox.Show("No Internet");
+                return;
+            }
             try
             {
                 lblStatus.Content = "Status : Reset";
 
-                var url = "http://autocontrol.freehost.io/reset.php"; // آدرس کامل کد PHP خود را اینجا قرار دهید
+                var url = "http://autocontrol.freehost.io/reset.php"; 
 
-                // ایجاد یک instance از HttpClient
                 using (var httpClient = new HttpClient())
                 {
-                    // ایجاد داده‌های فرم برای ارسال
                     var formData = new FormUrlEncodedContent(new[]
                     {
-                    new KeyValuePair<string, string>("email", txtUsername.Text), // جایگزین your_username با نام کاربری موردنظر خود کنید
-                    new KeyValuePair<string, string>("password", psbPassword.Password)  // جایگزین your_password با رمز عبور موردنظر خود کنید
+                    new KeyValuePair<string, string>("email", txtUsername.Text), 
+                    new KeyValuePair<string, string>("password", txtPassword.Text)  
                 });
 
                     // ارسال درخواست POST و دریافت پاسخ
@@ -149,6 +196,10 @@ namespace AutoControl
                         await Task.Delay(4000);
                         ShutDown();
                     }
+                    else
+                    {
+                        MessageBox.Show("Not Reset");
+                    }
                 }
             }
 
@@ -157,8 +208,42 @@ namespace AutoControl
                 MessageBox.Show($"Trouble resetting: {ex.Message}");
             }
         }
+        private void ShutDown()
+        {
+            if (checkbox_SaveInfo.IsChecked == true)
+            {
+                Settings.Default.Username = txtUsername.Text;
+                Settings.Default.Save();
+            }
+            try
+            {
+                if (rbtnHibernet.IsChecked == true)
+                {
+                    ProcessStartInfo psi = new ProcessStartInfo("shutdown", "/h");
+                    psi.CreateNoWindow = true;
+                    psi.UseShellExecute = false; 
+                    Process.Start(psi); 
+                }
+                else
+                {
+                    ProcessStartInfo psi = new ProcessStartInfo("shutdown", "/s /t 0 /f");
+                    psi.CreateNoWindow = true;
+                    psi.UseShellExecute = false; 
+                    Process.Start(psi); 
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Trouble shutting down: {ex.Message}");
+            }
+        }
         private async void Reset2()
         {
+            if (InternetCheck.Internet_test() == false)
+            {
+                MessageBox.Show("No Internet");
+                return;
+            }
             try
             {
                 lblStatus.Content = "Status : Reset";
@@ -172,7 +257,7 @@ namespace AutoControl
                     var formData = new FormUrlEncodedContent(new[]
                     {
                     new KeyValuePair<string, string>("email", txtUsername.Text), // جایگزین your_username با نام کاربری موردنظر خود کنید
-                    new KeyValuePair<string, string>("password", psbPassword.Password)  // جایگزین your_password با رمز عبور موردنظر خود کنید
+                    new KeyValuePair<string, string>("password", txtPassword.Text)  // جایگزین your_password با رمز عبور موردنظر خود کنید
                 });
 
                     // ارسال درخواست POST و دریافت پاسخ
@@ -190,56 +275,51 @@ namespace AutoControl
             }
         }
 
-        private void ShutDown()
-        {
-            try
-            {
-                Process[] processes = Process.GetProcesses();
-
-
-                foreach (Process process in processes)
-                {
-                    // چک کردن این که آیا برنامه دارای پنجره‌ای قابل دیدن است
-                    if (!String.IsNullOrEmpty(process.MainWindowTitle))
-                    {
-                        if (process.MainWindowTitle == this.Title)
-                        {
-
-                        }
-                        else if (process.MainWindowTitle == "Windows Explorer")
-                        {
-
-                        }
-                        else
-                        {
-                            process.CloseMainWindow();
-                        }
-                    }
-                }
-
-                ProcessStartInfo psi = new ProcessStartInfo("shutdown", "/s /t 2");
-                psi.CreateNoWindow = true; // بدون نمایش پنجره خط فرمان
-                psi.UseShellExecute = false; // استفاده از محیط غیرهمزمان
-                Process.Start(psi); // اجرای دستورات خاموش کردن سیستم
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"Trouble shutting down: {ex.Message}");
-            }
-        }
-        private void btnStop_Click(object sender, RoutedEventArgs e)
-        {
-            check = false;
-        }
-
         private void btnClose_Click(object sender, RoutedEventArgs e)
         {
-            if (psbPassword.Password.Trim() == "")
+            if (txtPassword.Text.Trim() == "")
             {
-                MessageBox.Show("insert Password");
-                return;
+                MessageBoxResult Result;
+                Result =  MessageBox.Show("insert Password","Warning",MessageBoxButton.OKCancel,MessageBoxImage.Stop);
+                if (Result== MessageBoxResult.OK)
+                {
+                    return;
+                }
+                else
+                {
+                    this.Close();
+                }
             }
-            this.Close();
+            if (txtUsername.Text.Trim() == "")
+            {
+                MessageBoxResult Result;
+                Result = MessageBox.Show("insert Username", "Warning", MessageBoxButton.OKCancel, MessageBoxImage.Stop);
+                if (Result == MessageBoxResult.OK)
+                {
+                    return;
+                }
+                else
+                {
+                    this.Close();
+                }
+            }
+            else
+            {
+                Reset2();
+                this.Close();
+            }
+        }
+
+        private void btnMore_Click(object sender, RoutedEventArgs e)
+        {
+            if (More.Visibility ==Visibility.Visible)
+            {
+                More.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                More.Visibility = Visibility.Visible;
+            }
         }
     }
 }
